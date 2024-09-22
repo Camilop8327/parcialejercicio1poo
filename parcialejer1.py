3f8df3efe52ec6499c9273491f4a79c1640cc98f
Camilop8327 <maria.lopez.bedoya@correounivalle.edu.co>
2024-09-21 19:25:54 -0500

    Add files via upload

---
+++ parcialejer1.py
@@ -0,0 +1,9 @@
+nume1 = int(input("ingrese el primer numero entero: "))
+nume2 = int(input("ingrese el segundo numero entero: "))
+
+suma = nume1 + nume2
+resta = nume1 - nume2
+multiplicacion = nume1 * nume2
+division = nume1 / nume2 
+
+print(f"Suma: {suma}, Resta: {resta}, Multiplicacion: {multiplicacion}, Division: {division}")
